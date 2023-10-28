7d952f59458ef5fbed462e920d18faf109f93b2d
Andrij Demchuk <stazi@ukr.net>
2023-10-28 15:27:08 +0300

    ThemesIcons

--- app/src/androidTestDebug/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTestDebug/java/com/example/tiptime/CalculatorTests.kt
@@ -1,6 +1,7 @@
 package com.example.tiptime
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -9,7 +10,6 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.matchers.JUnitMatchers.containsString
-//import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,6 +24,7 @@ class CalculatorTests {
     fun calculate_20_percent_tip() {
         onView(withId(R.id.cost_of_service))
             .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
 
         onView(withId(R.id.calculate_button)).perform(click())
 
